73594833c2535639fdd9befd68fb6858882970cc
Raunit Verma <ramgopal10258@gmail.com>
2022-08-05 12:47:13 +0530

    Create ajay.js

---
+++ ajay.js
@@ -0,0 +1,2 @@
+const ajay=document.querySelector('.gist-meta');
+ajay.innerHTML="Made with ❤ by <a href=\"https://www.linkedin.com/in/iraunit/\">Raunit Verma</a> <a href=\"https://play.google.com/store/apps/details?id=com.shyptsolution.codingkaro\" style =\"float:right\">Rate Us 5⭐ Here</a>"
